a9057ca25f38193930cb3234787d39fb87a72ef1
IsaacSG <54876928+IsaacSG@users.noreply.github.com>
2019-09-28 18:52:34 -0300

    Add files via upload

---
+++ SuperMercado2.0/src/ModelBeans/VendaBeans.java
@@ -0,0 +1,74 @@
+  *
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ModelBeans;
+import java.util.Date;
+
+public class VendaBeans {
+    
+    private int numero;
+    private Date dataVenda;
+    private double valor;
+    private int pesquisa;
+
+    /**
+     * @return the id_pessoa
+     */
+    public int getId_pessoa() {
+        return numero;
+    }
+
+    /**
+     * @param numero the id_pessoa to set
+     */
+    public void setId_pessoa(int numero) {
+        this.numero = numero;
+    }
+
+    /**
+     * @return the dataVenda
+     */
+    public Date getDataVenda() {
+        return dataVenda;
+    }
+
+    /**
+     * @param dataVenda the dataVenda to set
+     */
+    public void setDataVenda(Date dataVenda) {
+        this.dataVenda = dataVenda;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValorVenda() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValorVenda(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the pesquisa
+     */
+    public int getPesquisa() {
+        return pesquisa;
+    }
+
+    /**
+     * @param pesquisa the pesquisa to set
+     */
+    public void setPesquisa(int pesquisa) {
+        this.pesquisa = pesquisa;
+    }
+    
+    
+    
+}
