bb51b81cb68ea5230514a1bd42ce0c54877ec706
CaioVSG <54563549+caiovsg@users.noreply.github.com>
2019-11-11 12:26:44 -0300

    Implementado o método verificar em UsuarioDAO, PessoaFisicaDAO, PessoaJuridicaDAO e ProdutoDAO

--- SuperMercado2.0/src/ModelDao/ProdutoDAO.java
+++ SuperMercado2.0/src/ModelDao/ProdutoDAO.java
@@ -117,7 +117,7 @@ public class ProdutoDAO extends CadastroProdutos{
     public boolean validadorProduto(ProdutoBeans produto){
         char[] especiais = {'#', '@', '%', '&', '*', '(', ')', '+', '-', '$', '!', '?', '/', '|', '=', '§', '¹', '²', '³', '£', '*', '-', ',', '<', '>', '.', ';', ':'};
         char[] numeros = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
-        for (int i = 0; i < produto.getNome().length();i++){ //Não conseguir fazer verificação no id pq está em string
+        for (int i = 0; i < produto.getNome().length();i++){ //Não consegui fazer verificação no id pq está em string
             for (int k = 0; k < especiais.length;k++){
                 if(produto.getNome().charAt(i) == especiais[k]){
                     return false;
